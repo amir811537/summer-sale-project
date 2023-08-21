c44434efaccc32c796d790424fb591bdfb74def5
amir811537 <amirhossain.bc.75@gmail.com>
2023-08-21 21:25:45 +0600

    all done

--- js/summer.js
+++ js/summer.js
@@ -1,6 +1,8 @@
 const couponInput = document.getElementById("applycoupon");
 const applyButton = document.getElementById("couponcodebtn");
 const discountButton = document.getElementById("applydiscount");
+let total = 0;
+let discount = 0;
 
 // SELL200 condition
 couponInput.addEventListener("input", () => {
@@ -12,32 +14,37 @@ couponInput.addEventListener("input", () => {
 });
 
 applyButton.addEventListener("click", () => {
-  if (couponInput.value === "SELL200") {
-    alert("Coupon applied successfully!");
+  if (couponInput.value === "SELL200" && total >= 200) {
+    let discountTxt = document.getElementById("discountTxt");
+    discount = (total * 0.2).toFixed(2);
+    discountTxt.innerText = discount + " TK";
+
+    // Update final total after applying the discount
+    const finalTotal = document.getElementById("finalTotal");
+    finalTotal.innerText = total - discount + " TK";
   } else {
-    alert("Invalid coupon code. Please enter a valid code.");
+    alert("Invalid coupon code or total not eligible for discount.");
   }
 });
 
-let total=0;
 function handleCLikBtn(target) {
-  const selectedItemsContainer = document.getElementById('selected-items');
-  const itemName = target.querySelector('h3');
+  const selectedItemsContainer = document.getElementById("selected-items");
+  const itemName = target.querySelector("h3");
   const cardName = itemName.innerText;
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   li.innerText = cardName;
   selectedItemsContainer.appendChild(li);
-  const priceText = target.querySelector('p').innerText; // e.g., "39.00 TK"
+  const priceText = target.querySelector("p").innerText; // e.g., "39.00 TK"
   const price = parseFloat(priceText.split(" ")[0]); // Extract the numeric part
+
   total = total + price;
- const firstTotal =document.getElementById('Total').innerText=total;
- 
 
+  const firstTotal = (document.getElementById("Total").innerText = total);
+  const finalTotal = document.getElementById("finalTotal");
+  finalTotal.innerText = total + " tk";
 
-
-
-
+  if (total >= 200) {
+    let purchaseBtn = document.getElementById("purchaseBtn");
+    purchaseBtn.disabled = false;
   }
-
-
-
+}
