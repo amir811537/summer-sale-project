de734727c7318490cbbbab1c371b6fd71f370661
amir811537 <amirhossain.bc.75@gmail.com>
2023-08-21 22:14:13 +0600

    all set

--- js/summer.js
+++ js/summer.js
@@ -1,6 +1,6 @@
 const couponInput = document.getElementById("applycoupon");
 const applyButton = document.getElementById("couponcodebtn");
-const discountButton = document.getElementById("applydiscount");
+const makePurchaseButton = document.getElementById("purchaseBtn");
 let total = 0;
 let discount = 0;
 
@@ -15,13 +15,13 @@ couponInput.addEventListener("input", () => {
 
 applyButton.addEventListener("click", () => {
   if (couponInput.value === "SELL200" && total >= 200) {
-    let discountTxt = document.getElementById("discountTxt");
     discount = (total * 0.2).toFixed(2);
+    const discountTxt = document.getElementById("discountTxt");
     discountTxt.innerText = discount + " TK";
 
     // Update final total after applying the discount
     const finalTotal = document.getElementById("finalTotal");
-    finalTotal.innerText = total - discount + " TK";
+    finalTotal.innerText = (total - discount).toFixed(2) + " TK";
   } else {
     alert("Invalid coupon code or total not eligible for discount.");
   }
@@ -34,17 +34,22 @@ function handleCLikBtn(target) {
   const li = document.createElement("li");
   li.innerText = cardName;
   selectedItemsContainer.appendChild(li);
-  const priceText = target.querySelector("p").innerText; // e.g., "39.00 TK"
-  const price = parseFloat(priceText.split(" ")[0]); // Extract the numeric part
+  const priceText = target.querySelector("p").innerText;
+  const price = parseFloat(priceText.split(" ")[0]); 
 
   total = total + price;
-
   const firstTotal = (document.getElementById("Total").innerText = total);
   const finalTotal = document.getElementById("finalTotal");
-  finalTotal.innerText = total + " tk";
+  finalTotal.innerText = total.toFixed(2) + " tk";
 
+  // Enable "Have a coupon?" button when final total is 200 or more
   if (total >= 200) {
-    let purchaseBtn = document.getElementById("purchaseBtn");
-    purchaseBtn.disabled = false;
+    const couponButton = document.getElementById("couponcodebtn");
+    couponButton.removeAttribute("disabled");
+  }
+
+  // Enable "Make Purchase" button when total is 1 taka or more
+  if (total >= 1) {
+    makePurchaseButton.removeAttribute("disabled");
   }
 }
